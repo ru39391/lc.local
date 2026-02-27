b875b4326e2e7cb4be8f9486c2d86f59f92bc5a8
ru39391 <dev@39391.ru>
2026-02-27 11:50:47 +0300

    feature/реализовать разметку для навигации

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,5 +1,46 @@
+import Twig, { Template } from 'twig';
+
+const renderData = (tpl: Template): Node[] => {
+  const parser = new DOMParser();
+
+  const { body } = parser.parseFromString(
+    tpl.render(),
+    'text/html'
+  );
+
+  return Array.from(body.children);
+}
+
+const fetchTemplate = async (): Promise<Template | undefined> => {
+  try {
+    const res = await fetch('src/assets/templates/tpl.twig');
+    const data = await res.text();
+
+    return Twig.twig({ data });
+  } catch(err) {
+    console.error(err);
+  }
+}
+
 const init = () => {
   console.log("init");
 };
 
-export { init };
+const initApp = async () => {
+  const wrapper = document.querySelector<HTMLDivElement>('#app');
+
+  try {
+    const tpl = await fetchTemplate();
+    const arr = renderData(tpl as Template);
+
+    arr.forEach(item => wrapper?.append(item));
+    init();
+  } catch(err) {
+    console.error(err);
+  }
+};
+
+export {
+  init,
+  initApp
+};
